0833be089e3d6575fb5de2f220d37bea6f1075f9
i-brnrd <irmb@st-andrews.ac.uk>
2021-11-06 18:31:02 +0000

    Add files via upload

---
+++ melanin_sunscreen/sunscreen_thesis/no_grid/get_cdf.f90
@@ -0,0 +1,34 @@
+      
+subroutine get_cdf(c,spec,ndim)
+  !-------------------------------------------------
+  !     returns solar spectrum (CDF) and wavelengths
+  !-------------------------------------------------
+  implicit none
+  
+  integer, intent(in):: ndim
+  real*8, intent(in):: spec(2,ndim)
+  real*8, intent(inout):: c(ndim)
+  real*8 :: f(ndim),l(ndim)
+
+  integer :: i
+
+  !initialise each array
+  do i=1,ndim
+     l(i)=0.
+     f(i) = 0.
+     c(i) = 0.
+  enddo
+
+  l(:)=spec(1,:)
+  f(:)=spec(2,:)
+ 
+  !numerical integration to produce cumulative distribution
+  do i=1,ndim-1
+     c(i+1)= c(i) + 0.5*( f(i+1) + f(i) ) * ( l(i+1) - l(i) )
+  enddo
+ 
+  !normalise cumulative distribution to get cdf
+  c=c/c(ndim)
+ 
+  return 
+end subroutine get_cdf
