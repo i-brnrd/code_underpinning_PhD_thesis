6ea70c16d845d470c3094dd112db5473c92b508a
i-brnrd <irmb@st-andrews.ac.uk>
2021-11-06 18:24:39 +0000

    Add files via upload

---
+++ cpd_dna_thesis/optical_properties_init.f90
@@ -0,0 +1,114 @@
+module optical_properties_init_mod
+  implicit none
+  save
+contains
+  !!!!!!N
+   !!!!ns=1.39-((wl-279)*0.0001)
+  subroutine optical_properties_init()
+    use optical_properties_mod, ONLY: nwl, nlayer,lcount,l,u_a_all,u_s_all,g_all,n_all,wl_start
+    use load_spec2_mod
+    use search_bisec_mod
+    use interpolate_mod
+    implicit none
+
+    real*8,dimension (nwl):: u_a_base, u_s_base,u_a_mel, u_a_epidermis,oxy_hgb,deoxy_hgb !base values from omlc
+    real*8 spec(nwl),wl
+    integer :: i_wl, layer
+    real*8 :: jaques_basal
+
+    character*70 :: filename
+    real*8 lg10
+    real*8 :: V_m, v_m_epi   !melanin volumes
+    real*8 n_mel !'how many' melanin volumetric cells
+    real*8 v_b,sO2, hgb
+
+
+
+    do i_wl=1,nwl
+      l(i_wl)= wl_start + real(i_wl) -1.
+
+    enddo
+
+
+
+        u_s_all=0.
+        u_a_all=0.
+        n_all=1.38
+
+        g_all=0.
+          lg10 = Log(10.)
+          V_m=0.02 !volume of melanin in total.
+          v_m_epi=0.04 !volume of melanin in epidermis
+          n_mel=(lcount(2)*v_m) ! number of 'melanin voxels' in epidermis
+          v_b=0.02   !blood volume only 2% in upper skin
+          SO2=0.75
+          hgb=150./66500.
+
+      do i_wl=1,nwl
+        wl=l(i_wl)
+        g_all(i_wl)=0.62+0.29*wl*(10.**(-3))
+        u_a_base(i_wl)=7.84*(10.**8)*(wl**(-3.255))  !??'Base' Values; Jaques et al 1998 skin optics OMLC News
+        u_s_base(i_wl)=1.752*(10.**8)*(wl**(-2.33)) + 134.67*(wl**(-0.494))  !??'Base' Values; Jaques et al 1998 skin optics OMLC News
+        u_a_mel(i_wl)=6.6*(10.**11)*(wl**(-3.33))
+      enddo
+
+    layer=1 !STRATUM CORNEUM
+    !Absorption
+    filename='./spectra/absorption_sc.txt' !Van Gemert et al, 1989 (taken from Everett....)
+    call load_spec2(filename,spectrum=spec,unit_to_cm=10.d0)
+    u_a_all(layer,:)=spec
+    !scattering
+    filename='./spectra/scattering_sc.txt'  !Van Gemert et al, 1989
+    call load_spec2(filename,spec,10.d0)
+    u_s_all(layer,:)=spec
+    !--------------------------------------------------
+
+    layer =2 !EPIDERMIS
+    !Absorption
+    filename='./spectra/absorption_epi.txt'!Van Gemert
+    call load_spec2(filename,u_a_epidermis,10.d0)
+    u_a_all(layer,:)=u_a_epidermis-(v_m_epi*u_a_mel)
+    !Scattering
+    filename='./spectra/scattering_epi.txt' !Van Gemert
+    call load_spec2(filename,spec,10.d0)
+    u_s_all(layer,:)=spec
+    !-------------------------------------------------------
+
+    layer=3 !MELANIN LAYER
+    !Absorption
+    u_a_all(layer,:)= u_a_epidermis + (n_mel/real(lcount(3))) * u_a_mel
+    !Scattering
+    u_s_all(layer,:)=u_s_all(2,:)
+    !-------------------------------------------------------
+
+    layer=4 !DNA LAYER DNA: units?? is it out by 1000???
+    !Absorption
+    filename='./spectra/dna_molar_extinct.txt' !Mouret et al...
+    call load_spec2(filename,spec,1.d0)
+    u_a_all(layer,:)=lg10*0.0185*spec
+    u_a_all(layer,:)=u_a_all(2,:)
+    !Scattering
+    u_s_all(layer,:)=u_s_all(2,:)
+    !-------------------------------------------------------
+    layer = 5 !DERMIS
+    !Absorption
+    filename='./spectra/ohb_as.txt'
+    call load_spec2(filename,spec,1.d0)
+    oxy_hgb=spec
+
+    filename='./spectra/dhb_as.txt'
+    call load_spec2(filename,spec,1.d0)
+    deoxy_hgb=spec
+    u_a_all(layer,:)=(v_b*hgb*(SO2*oxy_hgb + (1.d0-sO2)*deoxy_hgb)*lg10)+u_a_base*(1.d0-v_b)
+
+    !scattering
+
+    filename='./spectra/scattering_epi_derm.txt' !Van Gemert
+    call load_spec2(filename,spec,10.d0)
+    !u_s_all(layer,:)=u_s_all(2,:)
+    u_s_all(layer,:)=spec
+
+    return
+  END SUBROUTINE optical_properties_init
+
+end module optical_properties_init_mod
---
+++ cpd_dna_thesis/pl_estimators.f90
@@ -0,0 +1,148 @@
+module pl_estimators_mod
+  implicit none
+  save
+contains
+
+
+!uses Lucy PL estimators to gather raw data from simulation results
+!--------------------------------------------------------------------------------
+  SUBROUTINE PL_ESTIMATORS(pkt_count,incident_spec_irr)
+    use optical_properties_mod, only: n_pkt_wl, l, nwl
+    use grid_mod, only: nxg,nyg,nzg,xmax,ymax,zmax, PL_SUM,MASK
+    use constants_mod
+    implicit none
+
+    integer, intent(in) :: pkt_count
+    real*8, intent(in) :: incident_spec_irr(nwl)
+
+    real*8 :: Area
+
+    !unnecessary:check?
+    real*8 :: wls(nwl)
+    real*8 :: lumin(nwl)
+    real*8 :: V
+    real*8, dimension(nxg,nyg,nzg) :: j_mean
+    real*8 :: l_norm, l_sum
+    integer :: i,j,k,m
+    real*8 :: depth_val(nzg)
+    real*8 :: picture_depths(nwl,nzg)
+
+    print*, nxg,nyg,nzg
+
+    wls=l
+    lumin=incident_spec_irr
+    l_sum=sum(incident_spec_irr)
+    print*, '****'
+    !print*, lumin
+    Area=(2.d0*(xmax))*(2.d0*(ymax))
+    V= (2.d0*(XMAX/NXG))*(2.d0*(YMAX/NYG))*(2.d0*(ZMAX/NZG))
+
+    print*,'Area',Area, ' in cm**2'
+    !L=L/(100.d0*100.d0) !W/cm
+    !gprint*,'Luminosity',L,'W/m^2'
+    print*,'Volume of Voxel',v
+    print*, 'cells', nxg,nyg,nzg
+
+    j_mean=0.d0
+    do m=1,nwl
+      if (n_pkt_wl(m).eq.0) cycle
+      l_norm=lumin(m)*area/(real(n_pkt_wl(m))*v)
+      do i=1,nxg
+        do j=1,nyg
+          do k=1,nzg
+            j_mean(i,j,k)=j_mean(i,j,k) + (PL_SUM(m,i,j,k))*l_norm
+          enddo
+        enddo
+      enddo
+    enddo
+
+    open(10,file='./data_out/depths_JAQUES.dat',status='replace')
+    depth_val=0.0d0
+    do k=1,nzg
+      do j=1,nyg
+        do i=1,nxg
+          !print*, PL_SUM(1,i,j,k)
+          depth_val(k)= depth_val(k) + j_mean(i,j,k)
+
+
+        enddo
+      enddo
+      depth_val(k)=depth_val(k)/(nxg*nyg)
+      write(10,*) (real(nzg-k)*(2.d0*zmax/real(nzg))), depth_val(k)/L_sum
+    !print*, (real(nzg-k)*(2.d0*zmax/real(nzg))), depth_val(k)/L_sum
+    enddo
+    close(10)
+
+    do m=1,nwl
+      if (n_pkt_wl(m).eq.0) then
+        PL_SUM(m,:,:,:)=0.d0
+      else
+        l_norm=lumin(m)*area/(real(n_pkt_wl(m))*v)
+        PL_SUM(m,:,:,:)=l_norm*PL_SUM(m,:,:,:)
+      endif
+    enddo
+
+
+
+
+    print*, 'Writing out the MASK'
+    open(10,file='./data_out/densitygrid.dat',status='replace',form='unformatted')
+    write(10) MASK
+    close(10)
+
+
+
+    print*, '...ABOUT TO WRITE TO THE PL FILE'
+      open(10,file='./data_out/pathlengths.dat',status='replace',form='unformatted')
+
+        write(10)  PL_SUM
+
+        close(10)
+        print*, 'written it out!!!!'
+
+    picture_depths=0.d0
+    do k=1,nzg
+      do j=1,nyg
+        do i=1,nxg
+          do m=1,nwl
+          picture_depths(m,k)= picture_depths(m,k) + PL_SUM(m,i,j,k)
+          enddo
+        enddo
+      enddo
+      picture_depths(:,k)=picture_depths(:,k)/(nxg*nyg)
+      write(11,*) (real(nzg-k)*(2.d0*zmax/real(nzg))), depth_val(k)/L_sum
+    enddo
+
+    print*, '...ABOUT TO WRITE TO THE PICTURE FILE'
+    open(10,file='./data_out/wavelengths_depths.dat',status='replace')
+    do k=1,NZG
+      write(10,*)  picture_depths(:,k)
+    enddo
+    close(10)
+    print*, 'written it out!!!!'
+
+
+
+    !I would still like to do this.....
+    ! print out values at each depth
+    open(10,file='./data_out/depths2.dat',status='replace')
+
+    depth_val=0.0d0
+    do k=1,nzg
+      do j=1,nyg
+        do i=1,nxg
+          !print*, PL_SUM(1,i,j,k)
+          depth_val(k)= depth_val(k) + j_mean(i,j,k)
+        enddo
+      enddo
+      depth_val(k)=depth_val(k)/(nxg*nyg)
+      write(10,*) (real(nzg-k)*(2.d0*zmax/real(nzg))), depth_val(k)/L_sum
+    !print*, (real(nzg-k)*(2.d0*zmax/real(nzg))), depth_val(k)/L_sum
+    enddo
+
+    close(10)
+
+    RETURN
+  END SUBROUTINE PL_ESTIMATORS
+
+end module pl_estimators_mod
